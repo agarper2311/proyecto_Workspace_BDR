d109ab3af23f1a82009c5281eca4e1430a52a73f
Ángel <agarper2311@g.educaand.es>
2024-05-10 14:57:33 +0200

    Subida de archivos

--- Workspace_BDR/src/main/java/com/es/tema9/teoria1/Teoria1.java
+++ Workspace_BDR/src/main/java/com/es/tema9/teoria1/Teoria1.java
@@ -1,8 +1,37 @@
 package com.es.tema9.teoria1;
 
+import java.sql.*;
+
 public class Teoria1 {
     public static void main(String[] args) {
 
+        String url = "jdbc:mysql://localhost:3306/alumnos?user=root&password=";
+        try {
+
+            // Establecer la conexión
+            Connection connection = DriverManager.getConnection(url);
+
+            // Crear objeto de tipo statement
+            Statement statement = connection.createStatement();
+
+            // Lanzar Query
+            ResultSet resulset = statement.executeQuery("SELECT * FROM estudiantes");
+
+            // Leer ResulSet
+            while(resulset.next()){
+
+               String nombre= resulset.getString("nombre");
+                System.out.println("Nombre: "+nombre);
+
+            }
+
+            // Cerrar la conexión
+            connection.close();
+
+
+        }catch (SQLException  e){
+            e.printStackTrace();
+        }
 
 
     }
